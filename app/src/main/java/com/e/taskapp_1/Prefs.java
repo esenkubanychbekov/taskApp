e2e6a34fa858de0b72c7cba859ec652f6201ec80
esenkubanychbekov <esen@esen.kubanychbekov>
2019-07-03 22:08:09 +0600

    add SharedPreferance

---
+++ app/src/main/java/com/e/taskapp_1/Prefs.java
@@ -0,0 +1,4 @@
+package com.e.taskapp_1;
+
+public class Prefs {
+}
---
+++ app/src/main/java/com/e/taskapp_1/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+
+    private String title;
+    private String desc;
+
+    public Task(String title, String desc) {
+        this.title = title;
+        this.desc = desc;
+    }
+}
